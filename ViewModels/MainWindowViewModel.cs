3ad950c85a1813de214d67597787cef457407e9e
Федоров Евгений Андреевич <Viva_la_lumiere@mail.ru>
2021-02-24 00:36:20 +0700

    Навел порядок по первому уроку MVVM

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,9 +2,6 @@
 using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows;
 using CV19.Models;
@@ -14,41 +11,27 @@ namespace CV19.ViewModels
     internal class MainWindowViewModel : ViewModel
     {
 
-        private IEnumerable<DataPoint> _TestDataPoints;
-        public IEnumerable<DataPoint> TestDataPoints { get => _TestDataPoints; set => Set(ref _TestDataPoints, value); }
-
-
-
-        private IEnumerable<DataPoint> _TestDataPoints2;
-        public IEnumerable<DataPoint> TestDataPoints2 { get => _TestDataPoints2; set => Set(ref _TestDataPoints2, value); }
+        #region Перелистывание вкладок
+        public ICommand ChangeTabIndexCommand { get; }
+        private void OnChangeTabIndexCommandExecuted(object p)
+        {
+            if (p is null) return;
+            SelectedPageIndex += Convert.ToInt32(p);
+        }
+        private bool CanChangeTabIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
 
 
         private int PageCount = 9;
-
         private int _SelectedPageIndex;
         public int SelectedPageIndex
         {
             //get => _SelectedPageIndex;
 
-            get { if (_SelectedPageIndex > PageCount || _SelectedPageIndex <0) { return _SelectedPageIndex=0; } else { return _SelectedPageIndex; } } // нихуя я умный
+            get { if (_SelectedPageIndex > PageCount || _SelectedPageIndex < 0) { return _SelectedPageIndex = 0; } else { return _SelectedPageIndex; } } // нихуя я умный
 
             set => Set(ref _SelectedPageIndex, value);
         }
-
-
-    
-
-    public ICommand ChangeTabIndexCommand { get; }
-        private void OnChangeTabIndexCommandExecuted(object p)
-        {      
-            if (p is null) return;
-            SelectedPageIndex += Convert.ToInt32(p);
-
-         
-        }
-        private bool CanChangeTabIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
-
-
+        #endregion
 
         #region Заголовок окна
 
@@ -76,14 +59,25 @@ namespace CV19.ViewModels
         private bool CanCloseApplicationCommandExecute(object p) => true;
         #endregion
 
+        #region Тестовые данные для графиков
+        private IEnumerable<DataPoint> _TestDataPoints;
+        public IEnumerable<DataPoint> TestDataPoints { get => _TestDataPoints; set => Set(ref _TestDataPoints, value); }
+        private IEnumerable<DataPoint> _TestDataPoints2;
+        public IEnumerable<DataPoint> TestDataPoints2 { get => _TestDataPoints2; set => Set(ref _TestDataPoints2, value); }
+        #endregion
+
         public MainWindowViewModel()
         {
+
+            #region Комманды-лямбда комманды в приложении
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
             ChangeTabIndexCommand = new LambdaCommand(OnChangeTabIndexCommandExecuted, CanChangeTabIndexCommandExecute);
-
+            #endregion
+            //
+            // Шляпный код
+            //
             var data_points = new List<DataPoint>();
             var data_points2 = new List<DataPoint>();
-
             for (var x = 0d; x <= 360; x += 0.1)
             {
                 const double to_rad = Math.PI / 180;
@@ -91,9 +85,6 @@ namespace CV19.ViewModels
                 data_points.Add(new DataPoint { XValue = x, YValue = y });
             }
             TestDataPoints = data_points;
-
-
-
             for (var x = 0d; x <= 360; x += 0.1)
             {
                 const double to_rad = Math.PI / 180;
